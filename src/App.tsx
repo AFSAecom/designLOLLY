8e7b5491bc35617d44806aa5ac498e4f1553bf95
AFSAecom <afsaecom@gmail.com>
2025-08-02 05:24:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Catalogue from './pages/Catalogue'
 
 function App() {
   return (
     <Routes>
+      {/* Redirection automatique de la racine vers /catalogue */}
+      <Route path="/" element={<Navigate to="/catalogue" />} />
+
+      {/* Route du catalogue */}
       <Route path="/catalogue" element={<Catalogue />} />
     </Routes>
   )
