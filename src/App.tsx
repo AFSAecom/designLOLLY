8a89758fd148c1d6c7ba4a2621fd1a555bf67066
AFSAecom <afsaecom@gmail.com>
2025-08-02 02:55:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,15 @@
-import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
-import LoginPage from "./components/LoginPage";
-import CreateAccountPage from "./components/CreateAccountPage";
-import ClientDashboard from "./components/ClientDashboard";
-import ConseillereDashboard from "./components/ConseillereDashboard";
-import AdminDashboard from "./components/AdminDashboard";
-import routes from "tempo-routes";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Catalogue from './pages/Catalogue'
 
 function App() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <>
-        {/* Tempo routes must come first */}
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          {/* Client Routes */}
-          <Route path="/client" element={<ClientDashboard />} />
-          <Route path="/client/dashboard" element={<ClientDashboard />} />
-          <Route
-            path="/client/login"
-            element={<LoginPage userType="client" />}
-          />
-          <Route
-            path="/client/create-account"
-            element={<CreateAccountPage />}
-          />
-
-          {/* Conseillère Routes */}
-          <Route
-            path="/conseillere/login"
-            element={<LoginPage userType="conseillere" />}
-          />
-          <Route
-            path="/conseillere/dashboard"
-            element={<ConseillereDashboard />}
-          />
-
-          {/* Admin Routes */}
-          <Route path="/admin/login" element={<LoginPage userType="admin" />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
-          {/* Tempo route fallback */}
-          {import.meta.env.VITE_TEMPO === "true" && (
-            <Route path="/tempobook/*" />
-          )}
-        </Routes>
-      </>
-    </Suspense>
-  );
+    <Router>
+      <Routes>
+        <Route path="/catalogue" element={<Catalogue />} />
+        {/* Tu pourras ajouter d'autres routes ici plus tard */}
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
