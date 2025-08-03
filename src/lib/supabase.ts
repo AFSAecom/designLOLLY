48c80f1ff766edaaa9dfa2169a9b0ca49bac6c7e
AFSAecom <afsaecom@gmail.com>
2025-08-03 23:33:24 +0100

    chore: require Supabase env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/types/supabase';
 
-const SUPABASE_URL =
-  import.meta.env.VITE_SUPABASE_URL || 'https://lftouuybkowdqimpgrnr.supabase.co';
-const SUPABASE_ANON_KEY =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmdG91dXlia293ZHFpbXBncm5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwOTYzMzMsImV4cCI6MjA2OTY3MjMzM30.TFTdCZA9Ej8wpCTx5q0QS88qLAk_aKavdAiVLiwkPFM';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 
